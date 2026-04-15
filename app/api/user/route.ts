490715d5ec35764fa5515d3bb7b6064defdfc0f7
Yeselamsew Bekele <yesjobkb@gmail.com>
2026-04-15 11:11:44 +0300

    user

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
 import { StaffRole } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   try {
